6ec0a3a151e906e56c4697d1251a4cc840b50d71
KevinEsh <kevin_esh@outlook.com>
2020-11-11 14:09:39 -0600

    changes to simulation

--- uva_hunt/algorithms/percolation/main.cpp
+++ uva_hunt/algorithms/percolation/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 #include <bitset>
 #include "../../data_structures/union_find/union_find.h"
@@ -7,6 +8,7 @@
 using namespace std;
 
 #define MAX_BIT_SET (1 << 20)
+bitset<MAX_BIT_SET> image;
 
 void print_image(bitset<MAX_BIT_SET> image, int size)
 {
@@ -20,72 +22,94 @@ void print_image(bitset<MAX_BIT_SET> image, int size)
     }
 }
 
-int main()
+double simulation_percolation(double prob_cell, int simulations, int size, UnionFind &unionfind)
 {
+    int num_cells = size * size;
+    int top = num_cells;
+    int bottom = num_cells + 1;
+    double percolates = 0;
 
-    srand(time(NULL));
-
-    int cell;
-    int size = 100;
-    int simulations = 10000;
-    int top = size * size;
-    int num_cells = top;
-    int bottom = top + 1;
-    double sum_prob = 0;
-
-    UnionFind unionfind(num_cells + 2);
-    bitset<MAX_BIT_SET> image;
-
-    // Initializing montecarlo simulation
     for (int s = 0; s < simulations; s++)
     {
         unionfind.clear();
         image.reset();
-        double prob = 0;
-        //Two of this nodes will be virtual nodes connected to top and bottom respecticly
+
+        // Generation of a random image with probability 'p' of open cell
+        for (int cell = 0; cell < num_cells; cell++)
+        {
+
+            if (double(rand()) / double(RAND_MAX) <= prob_cell)
+            {
+                image[cell] = 1;
+                // Union cell with his neighbours
+                if ((cell % size) != 0 && image[cell - 1])
+                {
+                    unionfind.set_union(cell, cell - 1);
+                }
+                if (((cell + 1) % size) != 0 && image[cell + 1])
+                {
+                    unionfind.set_union(cell, cell + 1);
+                }
+                if ((cell - size) > 0 && image[cell - size])
+                {
+                    unionfind.set_union(cell, cell - size);
+                }
+                if ((cell + size) < num_cells && image[cell + size])
+                {
+                    unionfind.set_union(cell, cell + size);
+                }
+            }
+        }
+
+        // Two of this nodes will be virtual nodes connected to top and bottom respecticly
         for (int i = 0; i < size; i++)
         {
             // connecting first row (top) to virtual node N²
-            unionfind.set_union(top, i);
+            if (image[i])
+            {
+                unionfind.set_union(top, i);
+            }
             // connecting first row (top) to virtual node N²+1
-            unionfind.set_union(bottom, size * (size - 1) + i);
+            if (image[size * (size - 1) + i])
+            {
+                unionfind.set_union(bottom, size * (size - 1) + i);
+            }
         }
 
-        while (!unionfind.are_connected(top, bottom))
+        // Does this image percolate?
+        if (unionfind.are_connected(top, bottom))
         {
-            cell = rand() % num_cells;
-            if (image[cell])
-            {
-                continue;
-            }
-            image[cell] = 1;
-            prob++;
-
-            //Union cell with his neighbours
-            if ((cell % size) != 0 && image[cell - 1])
-            {
-                unionfind.set_union(cell, cell - 1);
-            }
-            if (((cell + 1) % size) != 0 && image[cell + 1])
-            {
-                unionfind.set_union(cell, cell + 1);
-            }
-            if ((cell - size) > 0 && image[cell - size])
-            {
-                unionfind.set_union(cell, cell - size);
-            }
-            if ((cell + size) < num_cells && image[cell + size])
-            {
-                unionfind.set_union(cell, cell + size);
-            }
+            percolates++;
         }
-        prob /= num_cells;
-        //cout << prob << endl;
-        sum_prob += prob;
+    }
+
+    return percolates / simulations;
+}
+
+int main()
+{
+    // Random seed
+    srand(time(NULL));
+
+    int size = 10;
+    int simulations = 10000;
+    double prob_cell = 0.7;
+    double prob_percolates = 0;
+
+    UnionFind unionfind(size * size + 2);
+
+    ofstream myfile;
+    myfile.open("simulation_percolation.txt");
+
+    // Initializing montecarlo simulation
+    for (double prob_cell = 0; prob_cell <= 1; prob_cell += 0.01)
+    {
+        prob_percolates = simulation_percolation(prob_cell, simulations, size, unionfind);
+        cout << prob_percolates << endl;
+        myfile << prob_cell << "," << prob_percolates << "\n";
     }
 
     //print_image(image, size);
-
-    cout << sum_prob / simulations << endl;
+    myfile.close();
     return 0;
 }
